b69e5c3917d8885f7b0d3816453a44a5c30d95d5
Phong Doan <phongdoan.dev@gmail.com>
2019-11-19 15:01:08 -0800

    added canvas and started on chart

--- js/app.js
+++ js/app.js
@@ -163,4 +163,25 @@ function clickHandler(event) {
 }
 generateImages();
 listCickResult();
-console.log('Selectproduct :', Selectproduct.all);
+
+var ctx = document.getElementById('myChart').getContext('2d');
+// eslint-disable-next-line no-undef, no-unused-vars
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+      lineTension: 0
+    }]
+  },
+
+  // Configuration options go here
+  options: {}
+});
