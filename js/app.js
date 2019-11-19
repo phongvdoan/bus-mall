b1fe011cfe842a86f7ce2435215e4805a3a43ea3
Phong Doan <phongdoan.dev@gmail.com>
2019-11-19 14:37:57 -0800

    Added vote event and result list

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
+'use strict';
 //global variables
-
+var firstImgElem = document.getElementById('firstImg');
+var secImgElem = document.getElementById('secImg');
+var thirdImgElem = document.getElementById('thirdImg');
+var firstNameElem = document.getElementById('firstImgName');
+var secNameElem = document.getElementById('secImgName');
+var thirdNameElem = document.getElementById('thirdImgName');
+var imgChoice = document.getElementById('image-choices');
+var imgElem = [];
+imgElem.push(firstImgElem, secImgElem, thirdImgElem);
+var imgNameElem = [];
+imgNameElem.push(firstNameElem, secNameElem, thirdNameElem);
+// console.log('imgElem :', imgElem);
+var imgGenerator = [];
+var randomArr = [];
 
 //contructor
 function Selectproduct(name, imgURL) {
@@ -7,33 +21,32 @@ function Selectproduct(name, imgURL) {
   this.imgURL = imgURL;
   this.clickCtr = 0;
   this.shownCtr = 0;
+
+  Selectproduct.all.push(this);
 }
 
 //add to contructor
-var products = [];
-products.push(new Selectproduct('bag', 'img/bag.jpg'));
-products.push(new Selectproduct('banana', 'img/banana.jpg'));
-products.push(new Selectproduct('bathroom', 'img/bathroom.jpg'));
-products.push(new Selectproduct('boots', 'img/boots.jpg'));
-products.push(new Selectproduct('breakfast', 'img/breakfast.jpg'));
-products.push(new Selectproduct('bubblegum', 'img/bubblegum.jpg'));
-products.push(new Selectproduct('chair', 'img/chair.jpg'));
-products.push(new Selectproduct('cthulhu', 'img/cthulhu.jpg'));
-products.push(new Selectproduct('dog-duck', 'img/dog-duck.jpg'));
-products.push(new Selectproduct('dragon', 'img/dragon.jpg'));
-products.push(new Selectproduct('pen', 'img/pen.jpg'));
-products.push(new Selectproduct('pet-sweep', 'img/pet-sweep.jpg'));
-products.push(new Selectproduct('scissors', 'img/scissors.jpg'));
-products.push(new Selectproduct('shark', 'img/shark.jpg'));
-products.push(new Selectproduct('sweep', 'img/sweep.png'));
-products.push(new Selectproduct('tauntaun', 'img/tauntaun.jpg'));
-products.push(new Selectproduct('unicorn', 'img/unicorn.jpg'));
-products.push(new Selectproduct('usb', 'img/usb.gif'));
-products.push(new Selectproduct('water-can', 'img/water-can.jpg'));
-products.push(new Selectproduct('wine-glass', 'img/wine-glass.jpg'));
-
-
-console.log('products :', products);
+Selectproduct.all = [];
+new Selectproduct('bag', 'img/bag.jpg');
+new Selectproduct('banana', 'img/banana.jpg');
+new Selectproduct('bathroom', 'img/bathroom.jpg');
+new Selectproduct('boots', 'img/boots.jpg');
+new Selectproduct('breakfast', 'img/breakfast.jpg');
+new Selectproduct('bubblegum', 'img/bubblegum.jpg');
+new Selectproduct('chair', 'img/chair.jpg');
+new Selectproduct('cthulhu', 'img/cthulhu.jpg');
+new Selectproduct('dog-duck', 'img/dog-duck.jpg');
+new Selectproduct('dragon', 'img/dragon.jpg');
+new Selectproduct('pen', 'img/pen.jpg');
+new Selectproduct('pet-sweep', 'img/pet-sweep.jpg');
+new Selectproduct('scissors', 'img/scissors.jpg');
+new Selectproduct('shark', 'img/shark.jpg');
+new Selectproduct('sweep', 'img/sweep.png');
+new Selectproduct('tauntaun', 'img/tauntaun.jpg');
+new Selectproduct('unicorn', 'img/unicorn.jpg');
+new Selectproduct('usb', 'img/usb.gif');
+new Selectproduct('water-can', 'img/water-can.jpg');
+new Selectproduct('wine-glass', 'img/wine-glass.jpg');
 
 //contructor properties for counter
 Selectproduct.voteCTR = 0;
@@ -42,40 +55,105 @@ Selectproduct.maxVote = 25;
 
 
 //funtion to create random images
-var randomImgGenerator = function (max) {
-  // var imgSelection = new Set();
-  var randomUniqueValue = [];
-  for (var i = 0; i < 3; i++) {
-    randomUniqueValue.push(Math.floor(Math.random() * Math.floor(max)));
-    //console.log('randomUniqueValue :', randomUniqueValue);
-    if (randomUniqueValue[i] === randomUniqueValue[(i + 1)] || randomUniqueValue[i] === randomUniqueValue[(i - 1)]) {
-      randomUniqueValue.pop();
-      i -= 1;
-      //console.log('randomUniqueValue :', randomUniqueValue);
-    }
-    for (var j = 0; j < randomUniqueValue.length; j++) {
-      var imgLocation = ['firstImg', 'secImg', 'thirdImg'];
-      document.getElementById(imgLocation[j]).src = products[randomUniqueValue[j]].imgURL;
-    }
+// var randomImgGenerator = function (max) {
+//   // var imgSelection = new Set();
+//   var randomUniqueValue = [];
+//   for (var i = 0; i < 3; i++) {
+//     randomUniqueValue.push(Math.floor(Math.random() * max));
+//     //console.log('randomUniqueValue :', randomUniqueValue);
+//     if (randomUniqueValue[i] === randomUniqueValue[(i + 1)] || randomUniqueValue[i] === randomUniqueValue[(i - 1)]) {
+//       randomUniqueValue.pop();
+//       i -= 1;
+//       //console.log('randomUniqueValue :', randomUniqueValue);
+//     }
+//     for (var j = 0; j < randomUniqueValue.length; j++) {
+//       var imgLocation = ['firstImg', 'secImg', 'thirdImg'];
+//       document.getElementById(imgLocation[j]).src = Selectproduct.all[randomUniqueValue[j]].imgURL;
+//     }
+//   }
+//   console.log('randomUniqueValue :', randomUniqueValue);
+
+// };
+
+function shuffle(array) {
+  var currentIndex = array.length, tempValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    tempValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = tempValue;
   }
-  console.log('randomUniqueValue :', randomUniqueValue);
+  return array;
 
-};
-var firstImgElem = document.getElementById('firstImg');
-var secImgElem = document.getElementById('secImg');
-var thirdImgElem = document.getElementById('thirdImg');
+}
 
-firstImgElem.addEventListener('click', clickHandler);
-secImgElem.addEventListener('click', clickHandler);
-thirdImgElem.addEventListener('click', clickHandler);
+function getRandomImg() {
+  if (randomArr.length === 0) {
+    randomArr = shuffle(Selectproduct.all.slice());
+  }
+  var arrPop = randomArr.pop();
+  // console.log('arrPop :', arrPop);
+  return arrPop;
+}
 
-function clickHandler(event){
-  var id = event.target.id;
+function generateImages() {
+  for (var i = 0; i < 3; i++) {
+    imgGenerator.push(getRandomImg());
+    imgElem[i].src = imgGenerator[i].imgURL;
+    imgNameElem[i].textContent = imgGenerator[i].name;
+  }
+}
 
-  // if (id == ''
+function addElement(tag, container, text) {
+  var element = document.createElement(tag);
+  container.appendChild(element);
+  element.textContent = text;
+  return element;
+}
+var resultList = document.getElementById('resultList');
+
+
+function listCickResult() {
+  var text = '';
+
+  for (var i = 0; i < Selectproduct.all.length; i++) {
+    text = `${Selectproduct.all[i].name} : ${Selectproduct.all[i].clickCtr} votes.`;
+    addElement('li', resultList, text);
+  }
 }
 
 
-randomImgGenerator(products.length);
+imgChoice.addEventListener('click', clickHandler);
 
 
+function clickHandler(event) {
+  var id = event.target.id;
+  console.log('id :', id);
+  if (id === 'firstImg') {
+    imgGenerator[0].clickCtr++;
+    console.log('imgGenerator[0].clickCtr :', imgGenerator[0].clickCtr);
+  } else if (id === 'secImg') {
+    imgGenerator[1].clickCtr++;
+    console.log('imgGenerator[1].clickCtr :', imgGenerator[1].clickCtr);
+  } else if (id === 'thirdImg') {
+    imgGenerator[2].clickCtr++;
+    console.log('imgGenerator[2].clickCtr :', imgGenerator[2].clickCtr);
+  }
+
+  console.log('Selectproduct :', Selectproduct.all);
+  // firstImgElem.src = '';
+  // secImgElem.src = '';
+  // thirdImgElem.src = '';
+  imgGenerator = [];
+  resultList.innerHTML = '';
+  generateImages();
+  listCickResult();
+
+
+  // function votingResults() {
+
+}
+generateImages();
+listCickResult();
+console.log('Selectproduct :', Selectproduct.all);
